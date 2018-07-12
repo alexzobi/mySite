8b947e7831d3ce297fe7915d539c0c4ea4f1d23e
Alex Sobiloff <asobiloff@gmail.com>
2018-07-12 16:29:23 -0700

    feat: connect form to email

--- server/routes/contact.js
+++ server/routes/contact.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var models = require('../models');
-var Page = models.Page;
+var nodemailer = require('nodemailer');
 module.exports = router;
 
 // /contact
@@ -11,5 +10,35 @@ router.get('/', function (req, res, next) {
 
 router.post('/', function (req, res, next) {
   console.log(req.body)
+  const name = `${String(req.body.name)}` || ""
+  const email = `${String(req.body.email)}` || "" 
+  const location = `${String(req.body.city)}, 
+                  ${String(req.body.state)}, 
+                  ${String(req.body.country)}`
+  const message = `${String(req.body.message)}`
+
+  const transporter = nodemailer.createTransport({
+    service: 'yahoo',
+    auth: {
+      user: process.env.SEND_EMAIL,
+      pass: process.env.PASSWORD
+    }
+  });
+
+  const mailOptions = {
+    from: process.env.SEND_EMAIL,
+    to: process.env.RECEIVE_EMAIL,
+    subject: 'But Why Website Visitor',
+    text: 
+      `From: ${name}\nEmail: ${email}\nLocation: ${location} \nMessage: ${message}`
+  };
+
+  transporter.sendMail(mailOptions, function(error, info){
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Email sent: ' + info.response);
+    }
+  });
   res.redirect('/')
 });
